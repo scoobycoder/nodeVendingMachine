ec881538825d14f6aa4d14459af4e05f9d115b43
Nate <nathan_halterman@gap.com>
2017-11-12 17:58:46 -0500

    checking money for candy of chips before dispensing

--- machine.js
+++ machine.js
@@ -4,15 +4,17 @@ let coins = []
 let items = []
 const prices = [{"candy": 50}, {"chips": 75}]
 const clearCoins = () => coins = []
+const COIN_VALUE_LOC = 1
+const PRICE_VALUE_LOC = 0
 
 export const resetMachine = () => {
     clearCoins()
     items = []
 }
 
-export const pricer = (item) => {
+export const priceItem = (item) => {
     const price = prices.filter((priceItem) => keys(priceItem)[0] == values(item)[0])
-    return values(price[0])[0]
+    return values(price[PRICE_VALUE_LOC])[PRICE_VALUE_LOC]
 }
 
 export const itemTray = () => items
@@ -23,8 +25,10 @@ export const coinReturn = () => {
     return localCoin 
 }
 
+const checkMoney = () => coins.reduce((total, value) => total + values(value)[COIN_VALUE_LOC], 0)
+
 export const insertCoin = (coin) => coin['type'] === 'penny' ? console.log('No Pennies!') : coins.push(coin)
-export const chips = () =>  items.push({"type": "chips"})
-export const candy = () =>  items.push({"type": "candy"})
+export const chips = () =>  priceItem({"type": "candy"}) <= checkMoney() ? items.push({"type": "chips"}) : console.log("MO Money!")
+export const candy = () =>  priceItem({"type": "candy"}) <= checkMoney() ? items.push({"type": "candy"}) : console.log("MO Money!")
 
 export default () => true
--- spec/vending_spec.js
+++ spec/vending_spec.js
@@ -1,49 +1,61 @@
 import chai from 'chai'
 let expect = chai.expect;
 let assert = chai.assert;
-import machine, { coinReturn, insertCoin, chips, candy, itemTray, resetMachine, pricer } from "../machine"
+import machine, { coinReturn, insertCoin, chips, candy, itemTray, resetMachine, priceItem } from "../machine"
 
 describe('Vending Machine', function(){
 
     beforeEach(() => resetMachine())
 
+    const createQuarter = () => ({type: 'quarter', value: 25})
+    const createDime = () => ({type: 'dime', value: 10})
+    const createPenny = () => ({type: 'penny', value: 1})
+    const insertTwoQuarters = (number) => { insertCoin(createQuarter()); insertCoin(createQuarter()) }
+      
     it('should vend', function(done){
           assert.equal(machine(), true);
           done()
     })
 
     it('should return the coin inserted', function(done){
-      insertCoin({type: 'quarter'})
-      assert.deepEqual(coinReturn(), [{"type":"quarter"}]);
+      insertCoin(createQuarter())
+      assert.deepEqual(coinReturn(), [{"type":"quarter", "value":25}]);
       done()
     })  
 
     it('should return all the coins inserted', function(done){
-      insertCoin({type: 'quarter'})
-      insertCoin({type: 'dime'})      
-      assert.deepEqual(coinReturn(), [{"type":"quarter"}, {"type":"dime"}]);
+      insertCoin(createQuarter())
+      insertCoin(createDime())      
+      assert.deepEqual(coinReturn(), [{"type":"quarter","value":25}, {"type":"dime","value":10}]);
       done();
     })
 
     it('should not take pennies', function(done){
-      insertCoin({type: 'penny'})
+      insertCoin(createPenny())
       assert.deepEqual(coinReturn(), []);
       done()
     })
 
     it('should vend chips', function(done) {
+      insertTwoQuarters()
+      insertCoin(createQuarter())
       chips()
       assert.deepEqual(itemTray(), [{"type":"chips"}])
       done()
     })
 
     it('should vend candy', function(done){
+      insertCoin(createQuarter())
+      insertCoin(createQuarter())
       candy()
       assert.deepEqual(itemTray(), [{"type":"candy"}])
       done()
     })
 
     it('should vend two items', function(done){
+      insertTwoQuarters()
+      insertTwoQuarters()
+      insertCoin(createQuarter())
       candy()
       chips()
       assert.deepEqual(itemTray(), [{"type":"candy"}, {"type":"chips"}])
@@ -51,19 +63,26 @@ describe('Vending Machine', function(){
     })
 
     it('should know candy is 50 cents', function(done){
-      assert.equal(pricer({"type":"candy"}), 50)
+      assert.equal(priceItem({"type":"candy"}), 50)
       done()
     })
 
     it('should know chips are 75 cents', function(done){
-      assert.equal(pricer({"type":"chips"}), 75)
+      assert.equal(priceItem({"type":"chips"}), 75)
       done()
     })
 
-    xit('should only vend items if enough money is entered', function(done){
-      insertCoin({type: 'quarter'})
+    it('should not vend candy if insufficent money is entered', function(done){
+      insertCoin(createQuarter())
       candy()
       assert.deepEqual(itemTray(), [])
+      done()
+    })
+
+    it('should vend candy if sufficent money is entered', function(done){
+      insertTwoQuarters()
+      candy()
+      assert.deepEqual(itemTray(), [{"type":"candy"}])
       done()
     })
 
